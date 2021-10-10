f21c3f47090bc48227d6d5cd8d52b551af649037
Saashvath <74235537+Saashvath@users.noreply.github.com>
2021-10-10 18:07:18 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+
+var Imgarray =["Saashvath Shankar.jpg, IMG_7323.jpg,IMG-0209.JPG"] 
+
+var Namesarray = "Saashvath Shankar,Sharanya Shankar,Sinthuja Shankar,Shankar Gounder"
+
+function button()
+{
+
+ 
+    if(Namesarray == 3)
+    {
+      i=0;
+    }
+
+    if(Imgarray == 3)
+    {
+      i=0;
+    }
+
+}
